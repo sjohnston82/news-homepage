ad19e2fc01e5c6e97b7e3e68ab68dd16fffb14fd
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-08-16 09:22:22 -0400

    first commit and theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "soft-red": "hsl(5, 85%, 63%)",
+        "off-white": "hsl(36, 100%, 99%)",
+        "grayish-blue": "hsl(233, 8%, 79%)",
+        "dark-grayish-blue": "hsl(236, 13%, 42%)",
+        "very-dark-blue": "hsl(240, 100%, 5%)",
+      },
+      text: {
+        body: "15px",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
